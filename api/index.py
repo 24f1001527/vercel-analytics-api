7857e9a3f7fce3d56c36d510212e7120b7b199d4
24f1001527 <24f1001527@ds.study.iitm.ac.in>
2026-01-31 19:06:49 +0530

    Allow all HTTP methods in CORS middleware

--- api/index.py
+++ api/index.py
@@ -10,8 +10,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
 
 # 🔷 Load telemetry.json from REPO ROOT
